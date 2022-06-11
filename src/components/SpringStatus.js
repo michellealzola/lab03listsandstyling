5815991df877922d4e9bcc483231c7e1436106b2
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2022-06-10 21:48:48 -0600

    ternary approach

--- src/components/SpringStatus.js
+++ src/components/SpringStatus.js
@@ -9,14 +9,33 @@ class springstatus extends Component {
       }
     }
   render() {
-      if(this.state.isTeaching)
-      {
-        return <div>Status of Teaching in Spring: true</div>
-      }
-      else
-      {
-        return <div>Status of Teaching in Spring: false</div>
-      }
+    return(
+      this.state.isTeaching ? 
+      <div>Status of Teaching in Spring: true</div> : 
+      <div>Status of Teaching in Spring: false</div>
+    )
+
+    // let message 
+    // if(this.state.isTeaching)
+    // {
+    //   message = <div>Status of Teaching in Spring: true</div>       
+    // }
+    // else
+    // {
+    //   message = <div>Status of Teaching in Spring: false</div>
+    // }
+
+    // return <div>{message}</div>
+
+
+      // if(this.state.isTeaching)
+      // {
+      //   return <div>Status of Teaching in Spring: true</div>
+      // }
+      // else
+      // {
+      //   return <div>Status of Teaching in Spring: false</div>
+      // }
     // return (
     //   <div>springstatus</div>
     // )
